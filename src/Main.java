8b8dd56c34905519563df3e722adaa7d1ca4608b
lokky55 <lokky55@mail.ru>
2022-04-26 23:32:21 +0300

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 200;  // Объявляю переменную для стоимости билета
-        int rubBonus = 20;      // Объявялю переменную для количества рублей для одной бонусной милли
+        int ticketPrice = 2012;  // Объявляю переменную для стоимости билета
+        int rubBonus = 20;      // Объявляю переменную для количества рублей за одну бонусную милю
 
         int amountBonus = ticketPrice / rubBonus; // Рассчитываю количество бонусных милль
         System.out.println("За покупку билета Вам начислено: " + amountBonus + " бонусов");
